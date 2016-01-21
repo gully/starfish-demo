2f12168bcf09a169e0086a54e0d6f08ed8e7de00
gully <igully@gmail.com>
2016-01-21 12:17:57 +0800

    get rid of current for now

--- current/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to starfish-demo&#8217;s documentation!"],titleterms:{demo:0,document:0,indic:0,starfish:0,tabl:0,welcom:0}})
